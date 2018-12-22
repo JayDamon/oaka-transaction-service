e3fc2ea52813284d25cd054e5e740b3bd85a1e15
DESKTOP-MUAA09M\JayDamon <jay.n.damon@gmail.com>
2018-12-22 07:54:30 -0500

    Initial Commit

---
+++ src/main/java/com/protean/moneymaker/oaka/OakaApplication.java
@@ -0,0 +1,14 @@
+package com.protean.moneymaker.oaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OakaApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/protean/moneymaker/oaka/OakaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.protean.moneymaker.oaka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OakaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
