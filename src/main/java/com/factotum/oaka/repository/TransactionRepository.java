fee2e5f0e2793e085c3e0a56b17685dd4a536d3e
jaydamon <Heron2128#>
2021-03-09 07:30:40 -0500

    Revert "Initial Mongo db"
    This reverts commit 51704514

--- src/main/java/com/factotum/oaka/repository/TransactionRepository.java
+++ src/main/java/com/factotum/oaka/repository/TransactionRepository.java
@@ -1,36 +1,30 @@
 package com.factotum.oaka.repository;
 
-import com.factotum.oaka.dto.TransactionBudgetSummary;
 import com.factotum.oaka.model.Transaction;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-import java.util.Set;
 
 @Repository
-public interface TransactionRepository extends ReactiveMongoRepository<Transaction, Long>
-//        , BudgetSummaryRepository
-{
+public interface TransactionRepository extends ReactiveCrudRepository<Transaction, Long>, BudgetSummaryRepository {
 
     Flux<Transaction> findAllByOrderByDateDesc();
 
-    @Query(value = "SELECT " +
-            "new com.factotum.oaka.dto.TransactionBudgetSummary(" +
-            "tt.transactionTypeName, " +
-            "month(t.date), year(t.date), " +
-            "ABS(SUM(t.amount))) " +
-            "FROM Transaction As t " +
-            "INNER JOIN t.transactionType as tt " +
-            "WHERE month(t.date) = :month " +
-            "   AND year(t.date) = :year " +
-            "   AND t.budgetId in :budgetIds " +
-            "   AND tt.id = :tTypeId " +
-            "GROUP BY month(t.date), year(t.date),  tt.transactionTypeName " +
-            "ORDER BY year(t.date), month(t.date), tt.transactionTypeName DESC")
-    Mono<TransactionBudgetSummary> getBudgetSummaries(
-            int year, int month, Set<Long> budgetIds, int tTypeId);
+//    @Query(value = "SELECT " +
+//            "new com.factotum.oaka.dto.TransactionBudgetSummary(" +
+//            "tt.transactionTypeName, " +
+//            "month(t.date), year(t.date), " +
+//            "ABS(SUM(t.amount))) " +
+//            "FROM Transaction As t " +
+//            "INNER JOIN t.transactionType as tt " +
+//            "WHERE month(t.date) = :month " +
+//            "   AND year(t.date) = :year " +
+//            "   AND t.budgetId in :budgetIds " +
+//            "   AND tt.id = :tTypeId " +
+//            "GROUP BY month(t.date), year(t.date),  tt.transactionTypeName " +
+//            "ORDER BY year(t.date), month(t.date), tt.transactionTypeName DESC")
+//    @Query("SELECT * FROM transaction")
+//    Mono<TransactionBudgetSummary> getBudgetSummaries(
+//            int year, int month, Set<Long> budgetIds, int tTypeId);
 
 }
