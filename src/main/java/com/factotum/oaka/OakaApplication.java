e678d57715f4135bcd8c76fe2ded14afa54d3ff5
= <Jay.n.damon@gmail.com>
2021-07-26 21:01:51 -0400

    fix: move web client config

--- src/main/java/com/factotum/oaka/OakaApplication.java
+++ src/main/java/com/factotum/oaka/OakaApplication.java
@@ -3,9 +3,6 @@ package com.factotum.oaka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -13,12 +10,6 @@ public class OakaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OakaApplication.class, args);
-    }
-
-    @Bean
-    @LoadBalanced
-    WebClient.Builder builder() {
-        return WebClient.builder();
     }
 
 }
