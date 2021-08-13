7f06e88b781dd3a78206e8e7c0388ad1cf95112b
= <Jay.n.damon@gmail.com>
2021-08-12 21:15:32 -0400

    fix(kubernetes): Use fabric8 client to avoid log error"
    The logs consistently output ReflectorRunnable error caused by "too old resource version".
    
    This is intended to fix it.

--- src/test/java/com/factotum/oaka/service/TransactionRetrievalServiceImplIT.java
+++ src/test/java/com/factotum/oaka/service/TransactionRetrievalServiceImplIT.java
@@ -7,21 +7,16 @@ import com.factotum.oaka.dto.ShortAccountDto;
 import com.factotum.oaka.dto.TransactionDto;
 import com.factotum.oaka.http.AccountService;
 import com.factotum.oaka.http.BudgetService;
-import com.factotum.oaka.model.Transaction;
 import com.factotum.oaka.util.SecurityTestUtil;
-import org.apache.commons.collections4.map.HashedMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.oauth2.jwt.Jwt;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
 
