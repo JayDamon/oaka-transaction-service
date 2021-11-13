f1052fc6de0d29f9c9184c1899aeca4ec9fb494e
= <Jay.n.damon@gmail.com>
2021-11-13 11:51:54 -0500

    refactor: Add docker login credentials

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 image=$1;
+docker_user=$2;
+docker_password=$3;
 
 version=$(git describe --tags --abbrev=0 | sed 's/v//')
 major_version=$(git describe --tags --abbrev=0 | cut -f1 -d"." | sed 's/v//')
@@ -12,7 +14,7 @@ echo $minor_version
 
 docker build -t $image:$version -t $image:$major_version -t $image:$minor_version -t $image:latest .
 
-docker login
+docker login -u $docker_user -p $docker_password
 
 docker push $image:$version
 docker push $image:$major_version
