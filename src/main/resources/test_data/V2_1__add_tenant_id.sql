d19ea0c3828ffc4c36626c2f4f25a4af2683ef65
jaydamon <jay.n.damon@gmail.com>
2022-02-06 09:02:24 -0500

    refactor: Update local/test data to match new keycloak user

--- src/main/resources/test_data/V2_1__add_tenant_id.sql
+++ src/main/resources/test_data/V2_1__add_tenant_id.sql
@@ -1,4 +1,4 @@
-update recurring_transaction SET tenant_id = '684996db-6cf8-4976-8336-6e664386dcda';
+update recurring_transaction SET tenant_id = '5809b48e-b705-4b3e-be9f-16ce0380cb45';
 
 INSERT INTO recurring_transaction (recurring_transaction_id, name, account_id, budget_category_id,
                                    frequency_type_id, frequency, occurrence_id,
@@ -6,7 +6,7 @@ INSERT INTO recurring_transaction (recurring_transaction_id, name, account_id, b
                                    end_date, amount, tenant_id)
 VALUES (4, 'Plex', 1, 10, 2, 1, 1, 70, 2, '2017-01-04', NULL, 3.99, '7c21bdec-219e-4492-a2fd-e6a2ec5fb8fa');
 
-update transaction SET tenant_id = '684996db-6cf8-4976-8336-6e664386dcda';
+update transaction SET tenant_id = '5809b48e-b705-4b3e-be9f-16ce0380cb45';
 
 INSERT INTO transaction (account_id, budget_id, transaction_category_id, transaction_type_id,
 recurring_transaction_id, transaction_date, description, amount, tenant_id)
